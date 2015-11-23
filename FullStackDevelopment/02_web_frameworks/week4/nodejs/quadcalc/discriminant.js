3125af8e1acde547cc810aa5237df1c15c5f8f7f
Ob1Ju4n <juan1018@gmail.com>
2015-11-23 03:01:22 -0500

    Week 4
    Includes bower components and JS code for controlling behavior of html
    components

--- FullStackDevelopment/02_web_frameworks/week4/nodejs/quadcalc/discriminant.js
+++ FullStackDevelopment/02_web_frameworks/week4/nodejs/quadcalc/discriminant.js
@@ -1,3 +1,6 @@
 /**
  * Created by Juan on 11/14/2015.
  */
+module.exports = function(a,b,c){
+    return(b*b - 4*a*c);
+}
--- FullStackDevelopment/02_web_frameworks/week4/nodejs/quadcalc/solve.js
+++ FullStackDevelopment/02_web_frameworks/week4/nodejs/quadcalc/solve.js
@@ -1,3 +1,22 @@
 /**
  * Created by Juan on 11/14/2015.
  */
+var quad = require('./quadratic');
+var prompt = require('prompt');
+
+prompt.get(['a','b','c'], function(err, result){
+        if(err){return (err);}
+        console.log("Command-line input received:");
+        console.log("a: "+result.a);
+        console.log("b: "+result.b);
+        console.log("c: "+result.c);
+
+        quad(result.a,result.b,result.c, function(err, quadresolve){
+            if(err){
+                console.log("Error: "+err);
+            }else{
+                console.log("Roots are "+quadresolve.root1()+" and  "+quadresolve.root2());
+            }
+        });
+});
+
